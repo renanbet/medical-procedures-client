b0e12ca4381bdda9d6b62599b8bdc1e0e9a99e84
renan bet rodrigues <renanbet@hotmail.com>
2020-04-29 18:42:58 -0300

    angular structure and login page html

---
+++ src/app/medical-procedures/medical-procedures.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { MedicalProceduresComponent } from './medical-procedures.component';
+
+describe('MedicalProceduresComponent', () => {
+  let component: MedicalProceduresComponent;
+  let fixture: ComponentFixture<MedicalProceduresComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ MedicalProceduresComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(MedicalProceduresComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/medical-procedures/medical-procedures.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-medical-procedures',
+  templateUrl: './medical-procedures.component.html',
+  styleUrls: ['./medical-procedures.component.scss']
+})
+export class MedicalProceduresComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
